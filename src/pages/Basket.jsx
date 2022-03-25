6a58bf9a0aac3a118981cf92d4c94a2b3156aeb0
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-03-25 14:29:49 +0100

    basket en oreder when user is null

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { validateUser, calculateItemPrice, calculateTotalPrice, postOrder, handleChange } from '../functions/Functions.jsx'
+import { validateUser, calculateItemPrice, calculateTotalPrice, postOrder, handleChange, signIn,  } from '../functions/Functions.jsx'
 import { Link } from 'react-router-dom'
 
 export default function Basket() {
@@ -54,14 +54,37 @@ export default function Basket() {
 
     if (user === null)
         return (
-            <div className="signOut-div">
-                <h1>Sing in to se your orders.</h1>
-                <button className="signOut-button">
-                    <Link to='/signIn'>
-                        SignIn
-                    </Link>
-                </button>
-            </div>
-        )
+            <div className="signIn-page">
+        <header className="signIn-header">
+          <h1>Drin</h1>
+        </header>
+
+        <div className="signIn-box">
+          <h1>Sign-In</h1>
+          <form className="signIn-form" id="signInForm"
+            onSubmit={e => {
+              e.preventDefault()
+              // @ts-ignore
+              const email = e.target.email.value
+              // @ts-ignore
+              const password = e.target.password.value
+              signIn(email, password, setUser)
+            }}
+          >
+            <label className="signIn-email-label" htmlFor="email">Email</label>
+            <input type="email" className="signIn-input" name="email"></input>
+            <label className="signIn-password-label" htmlFor="password">Password</label>
+            <input type="password" className="signIn-input" name="password"></input>
+            <button className="signIn-button" type="submit" form="signInForm">Sign In</button>
+          </form>
+        </div>
+        <div className="brake">
+          <h5>New to Drin?</h5>
+        </div>
+        <div>
+          <button className="signUp-button"> <Link to='/signUp'> Create your Drin account </Link></button>
+        </div>
+      </div>
+    )
 
 } 
